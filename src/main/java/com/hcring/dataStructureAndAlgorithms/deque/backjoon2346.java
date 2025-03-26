8286a1f6a8380d3487100a85f1507ec6fcb954e1
didiha <hcring@naver.com>
2025-03-26 16:01:55 +0900

    2025/03/26 백준 2346 풀기

--- src/main/java/com/hcring/dataStructureAndAlgorithms/deque/backjoon2346.java
+++ src/main/java/com/hcring/dataStructureAndAlgorithms/deque/backjoon2346.java
@@ -1,4 +1,23 @@
 package com.hcring.dataStructureAndAlgorithms.deque;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class backjoon2346 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        StringTokenizer st = new StringTokenizer(input);
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> deque = new LinkedList<Integer>();
+        for(int i = 1; i <= N; i++){
+            deque.add(i);
+        }
+
+    }
 }
