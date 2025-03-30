0852cd919ea1f95256b77993d25f4df131e384fe
didiha <hcring@naver.com>
2025-03-30 20:41:33 +0900

    2025/03/31 백준1202 문제 풀기

--- src/main/java/com/hcring/dataStructureAndAlgorithms/heap/Backjoon1202.java
+++ src/main/java/com/hcring/dataStructureAndAlgorithms/heap/Backjoon1202.java
@@ -1,25 +1,46 @@
 package com.hcring.dataStructureAndAlgorithms.heap;
 
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Backjoon1202 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringTokenizer st = new StringTokenizer(sc.nextLine());
 
-        String inputNK = sc.nextLine();
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[][] MV = new int[2][N];
-        int[] C = new int[K];
 
-        for (int i = 0; i <= N; i++) {
-            String inputMV = sc.nextLine();
-            MV[0][i] = Integer.parseInt(st.nextToken());
-            MV[1][i] = Integer.parseInt(st.nextToken());
+        int[][] jewels = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(sc.nextLine());
+            jewels[i][0] = Integer.parseInt(st.nextToken());
+            jewels[i][1] = Integer.parseInt(st.nextToken());
         }
 
+        int[] capacities = new int[K];
+        for (int i = 0; i < K; i++) {
+            capacities[i] = sc.nextInt();
+        }
 
+        Arrays.sort(jewels, (a, b) -> Integer.compare(b[0], a[0]));
+        Arrays.sort(capacities);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        long maxValue = 0;
+        int jewelIndex = 0;
+        for (int capacity : capacities) {
+            while (jewelIndex < N && jewels[jewelIndex][1] <= capacity) {
+                pq.offer(jewels[jewelIndex][0]);
+                jewelIndex++;
+            }
+
+            if (!pq.isEmpty()) {
+                maxValue += pq.poll();
+            }
+        }
+
+        System.out.println(maxValue);
     }
 }
