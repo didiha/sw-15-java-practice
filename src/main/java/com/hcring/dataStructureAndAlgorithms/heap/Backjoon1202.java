261fcbe268601d960d2b14cc45a043cc97c97e03
didiha <hcring@naver.com>
2025-04-01 22:39:47 +0900

    2025/04/01 백준1202 문제 풀기

--- src/main/java/com/hcring/dataStructureAndAlgorithms/heap/Backjoon1202.java
+++ src/main/java/com/hcring/dataStructureAndAlgorithms/heap/Backjoon1202.java
@@ -1,46 +1,69 @@
 package com.hcring.dataStructureAndAlgorithms.heap;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Backjoon1202 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringTokenizer st = new StringTokenizer(sc.nextLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+    static class Jewel implements Comparable<Jewel> {
+        int weight;
+        int value;
 
-        int[][] jewels = new int[N][2];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(sc.nextLine());
-            jewels[i][0] = Integer.parseInt(st.nextToken());
-            jewels[i][1] = Integer.parseInt(st.nextToken());
+        public Jewel(int weight, int value) {
+            this.weight = weight;
+            this.value = value;
         }
 
-        int[] capacities = new int[K];
-        for (int i = 0; i < K; i++) {
-            capacities[i] = sc.nextInt();
+        @Override
+        public int compareTo(Jewel other) {
+            return this.weight - other.weight; // 무게 오름차순
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Jewel[] jewels = new Jewel[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            jewels[i] = new Jewel(m, v);
         }
 
-        Arrays.sort(jewels, (a, b) -> Integer.compare(b[0], a[0]));
-        Arrays.sort(capacities);
+        int[] bags = new int[k];
+        for (int i = 0; i < k; i++) {
+            bags[i] = Integer.parseInt(br.readLine());
+        }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        Arrays.sort(jewels); // 보석 무게 오름차순 정렬
+        Arrays.sort(bags);   // 가방 무게 오름차순 정렬
 
-        long maxValue = 0;
+        long totalValue = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a); // 최대 힙 (가격 내림차순)
+
         int jewelIndex = 0;
-        for (int capacity : capacities) {
-            while (jewelIndex < N && jewels[jewelIndex][1] <= capacity) {
-                pq.offer(jewels[jewelIndex][0]);
+        for (int bagWeight : bags) {
+            // 현재 가방에 넣을 수 있는 보석들을 힙에 추가
+            while (jewelIndex < n && jewels[jewelIndex].weight <= bagWeight) {
+                pq.offer(jewels[jewelIndex].value);
                 jewelIndex++;
             }
 
+            // 힙이 비어있지 않다면, 가장 비싼 보석을 꺼내 가방에 넣음
             if (!pq.isEmpty()) {
-                maxValue += pq.poll();
+                totalValue += pq.poll();
             }
         }
 
-        System.out.println(maxValue);
+        System.out.println(totalValue);
     }
 }
